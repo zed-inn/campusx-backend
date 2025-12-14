7b751ff4b62228c4235d97fb17bd199a243bd1a9
zed <zed@zemail.com>
2025-12-15 01:50:30 +0530

    chore(validate): name change to specify body

--- src/shared/middlewares/validate-request.ts
+++ src/shared/middlewares/validate-request.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodObject } from "zod";
 
-export const validateRequest = (schema: ZodObject) => {
+export const validateRequestBody = (schema: ZodObject) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
       req.body = schema.parse(req.body);
