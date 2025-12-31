9f5d9d805148ead1e2e4d52bb980fcd74df98027
zed <zed@zemail.com>
2025-12-31 23:40:06 +0530

    feat: server scripts

---
+++ scripts/reload_server.sh
@@ -0,0 +1,8 @@
+echo "Pulling from the latest commit"
+git pull origin main
+echo "Pull complete"
+
+echo "Restarting application"
+npm i
+forever restart -s dist/app.js
+exit
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+forever start -s dist/app.js
+echo "Application started"
