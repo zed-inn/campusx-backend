794b80796ff840a2afe890d1c7cc00139fc0320f
zed <zed@zemail.com>
2026-01-12 03:22:55 +0530

    fix: events status updated

--- src/modules/features/competition/event/event.constants.ts
+++ src/modules/features/competition/event/event.constants.ts
@@ -3,6 +3,6 @@ import { createEnum } from "@shared/utils/create-enum";
 export const EVENT = {
   NAME: { LENGTH: { MIN: 1, MAX: 100 } },
   DESCRIPTION: { LENGTH: { MIN: 1, MAX: 1000 } },
-  STATUS: createEnum(["QUEUED", "ONGOOING", "ENDED"] as const),
+  STATUS: createEnum(["UPCOMING", "ONGOING", "ENDED"] as const),
   RULES: { HOW_MANY: { MIN: 0, MAX: 20 } },
 };
